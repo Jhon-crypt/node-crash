67619db85ee982b739013b853c00aa26ca73173f
Jhon-crypt <johnoladele690@gmail.com>
2024-08-20 21:27:05 +0100

    Nodemon

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ import http from 'http'
 
 const PORT = 8000
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) =>  {
     /*
     res.setHeader('Content-Type', 'text/html');
     res.statusCode = 404
     */
-    res.writeHead(200, { 'content-Type': 'application/json' })
+    //Json content type
+    /*
+    res.writeHead(200, { 'content-Type': 'text/json' })
     res.end(JSON.stringify({ message: 'Success' }))
+    */
+   res.writeHead(200, { 'content-Type': 'text/html' })
+   res.end('<h1>Welcome To NodeJs</h1>')
 });
 
 server.listen(PORT, () => {
