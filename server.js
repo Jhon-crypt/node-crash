54c44d189b492b08d9210611f74cdda943ba6c71
Jhon-crypt <johnoladele690@gmail.com>
2024-08-20 21:58:42 +0100

    Manual server router

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
 import http from 'http'
 
-const PORT = 8000
+const PORT = process.env.PORT
 
-const server = http.createServer((req, res) =>  {
+const server = http.createServer((req, res) => {
     /*
     res.setHeader('Content-Type', 'text/html');
     res.statusCode = 404
     */
+
     //Json content type
     /*
     res.writeHead(200, { 'content-Type': 'text/json' })
     res.end(JSON.stringify({ message: 'Success' }))
     */
-   res.writeHead(200, { 'content-Type': 'text/html' })
-   res.end('<h1>Welcome To NodeJs</h1>')
+
+    //Manual Router
+    if(req.url === "/") {
+
+        res.writeHead(200, { 'content-Type': 'text/html' })
+        res.end('<h1>Home Page</h1>');
+
+    } else if(req.url === "/about") {
+
+        res.writeHead(200, { 'content-Type': 'text/html' })
+        res.end('<h1>About Page</h1>')
+
+    } else if(req.url === "/contact") {
+
+        res.writeHead(200, { 'content-Type': 'text/html' })
+        res.end('<h1>Contact Page</h1>')
+
+    }else{
+        res.writeHead(404, { 'content-Type': 'text/html' })
+        res.end('<h1>Not Found</h1>')
+    }
+
+    //console.log("This is the request URL:",req.url)
+    //console.log("This is the request Method:",req.method)
+
 });
 
 server.listen(PORT, () => {
